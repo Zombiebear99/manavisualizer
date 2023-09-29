267569afbfd1c809c8d9e00273f47fd5cfaa6201
Deepakqw <59117886+Deepakqw@users.noreply.github.com>
2023-09-29 23:17:12 +0530

    adding tooltip to mana containg items

---
+++ src/main/java/org/zombie/manavisualizer/ManaNumberFormatting.java
@@ -0,0 +1,50 @@
+package org.zombie.manavisualizer;
+
+import java.text.DecimalFormat;
+
+public final class ManaNumberFormatting {
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#,###.##");
+
+    private static String numericalDisplay(int mana) {
+        return String.format("%,d", mana);
+    }
+
+    private static String shortenedNumericalDisplay(int mana) {
+        String abbreviation;
+        double shortenedMana = mana;
+        if (mana >= 1000000000) {
+            abbreviation = "B";
+            shortenedMana /= 1000000000;
+        } else if (mana >= 1000000) {
+            abbreviation = "M";
+            shortenedMana /= 1000000;
+        } else if (mana >= 1000) {
+            abbreviation = "K";
+            shortenedMana /= 1000;
+        } else {
+            return Integer.toString(mana);
+        }
+        return DECIMAL_FORMAT.format(shortenedMana) + abbreviation;
+    }
+
+    public static String amount(int mana) {
+        return numericalDisplay(mana);
+    }
+
+    private static String percentageDisplay(int mana, int maxMana) {
+        return DECIMAL_FORMAT.format(((double) mana / maxMana) * 100);
+    }
+
+    public static String percentage(int mana, int maxMana) {
+        return percentageDisplay(mana, maxMana);
+    }
+
+    public static String capacity(int mana, int maxMana) {
+        return String.format(
+                "Mana: %s/%s (%s%%)",
+                shortenedNumericalDisplay(mana),
+                shortenedNumericalDisplay(maxMana),
+                percentageDisplay(mana, maxMana)
+        );
+    }
+}
---
+++ src/main/java/org/zombie/manavisualizer/mixin/BotaniaItemNumericalMixin.java
@@ -0,0 +1,39 @@
+package org.zombie.manavisualizer.mixin;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import vazkii.botania.api.mana.ManaItem;
+import vazkii.botania.common.item.ManaTabletItem;
+import vazkii.botania.common.item.equipment.bauble.BaubleItem;
+import vazkii.botania.xplat.XplatAbstractions;
+
+import java.util.List;
+
+@Mixin({
+		ManaTabletItem.class,
+		BaubleItem.class
+})
+public class BotaniaItemNumericalMixin extends Item
+{
+	public BotaniaItemNumericalMixin(Properties pProperties) {
+		super(pProperties);
+	}
+
+	@Override
+	public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> components, @NotNull TooltipFlag tooltipFlag) {
+		final ManaItem manaItem = XplatAbstractions.INSTANCE.findManaItem(stack);
+		if (manaItem != null && !ManaTabletItem.isStackCreative(stack)) {
+
+			components.add(Component.literal(String.format("Mana : %s / %s",manaItem.getMana(),manaItem.getMaxMana())).withStyle(ChatFormatting.AQUA));
+		}
+
+		super.appendHoverText(stack, level, components, tooltipFlag);
+	}
+}
